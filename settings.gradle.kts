369ad19d79733056ba4025951793c0eeafd01b01
lurve <129316709+lurve1314@users.noreply.github.com>
2026-06-28 21:18:41 +0800

    更新 settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,24 +4,14 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven { url = uri("https://www.jitpack.io") }
-        maven { url = uri("https://maven.aliyun.com/repository/releases") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/central") }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://www.jitpack.io") }
-        maven { url = uri("https://maven.aliyun.com/repository/releases") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/central") }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
+        maven { url = uri("https://www.jitpack.io") }
     }
 }
 
